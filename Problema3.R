e0b8a5ef553ef1b555a417e0c64b645d242b1dfe
gorka-ctrl <gorka.espinosa@estudiantat.upc.edu>
2025-02-18 10:45:38 +0100

    Update Problema3.R

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,19 @@
 #Codigo para problema 3
+
+#Recorrido(2,3,4)
+x[2:4]
+
+#Vector suma
+x<-c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+sum(x)
+# Eliminación dades
+y<-x[-c(21,2,26)]
+y
+#Calculo
+sum(exp(x)-sum(exp(y)))
+
+#TRUE O FALSE
+y[19]>=y[24]
+
+x[2:4]
+#Codigo para problema 3
