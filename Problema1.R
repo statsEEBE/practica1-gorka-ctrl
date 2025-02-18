cc755005329edf96b3df33fd045fa3066c55975d
gorka-ctrl <gorka.espinosa@estudiantat.upc.edu>
2025-02-18 09:44:33 +0100

    PRIMERA CLASE/ Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,46 @@
 #Codigo para problema 1
 
-plot(1:5)
+ 
+mis_dades<-mtcars
+mis_dades
+dim(mis_dades)
+names(mis_dades)
+
+#Variable de la tabla
+x<-mis_dades$qsec
+x
+
+# La media
+sum(x)/length(x)
+mean(x)
+
+#La Mediana
+mis_dades$drat
+sort(mis_dades$drat)
+median(mis_dades$drat)
+
+#Primer quartil
+quantile(mis_dades$drat,0.25)
+mis_dades$mpg
+sort(mis_dades$mpg)
+
+#Valor Percentil
+quantile(sort(mis_dades$mpg),0.18)
+
+#Rang Interquartile = 3ªquartile - 1ªquartile
+z<-quantile(mis_dades$cyl,0.25)
+d<-quantile(mis_dades$cyl,0.75)
+d-z
+
+#Rang interquartile Directe
+IQR(mis_dades$cyl)
+
+#Desviació Típica (corregida)
+sd(mis_dades$cyl)
+
+#Varianza corregida
+var(mis_dades$qsec)
+
+#Formula Varianza
+x<-mis_dades$qsec
+sum(((x-mean(x))^2)/(length(x)-1))
